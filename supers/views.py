7f5861faa49871aa2c8f078dc8935a8e0aaaaba3
unknown <qwhick2@gmail.com>
2022-02-18 12:46:48 -0500

    Created POST, PUT, DELETE Method Functionality

--- supers/views.py
+++ supers/views.py
@@ -14,8 +14,25 @@ def super_list(request):
         supers = Super.objects.all()
         serializer = SuperSerializer(supers, many=True)
         return Response(serializer.data)
+    
+    elif request.method == 'POST':
+        serializer = SuperSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
 @api_view(['GET', 'PUT', 'DELETE'])
-def super_detail(request):
-    pass
+def super_detail(request, pk):
+    super = get_object_or_404(Super, pk=pk)
+    if request.method == 'GET':
+        serializer = SuperSerializer(super)
+        return Response(serializer.data)
+    elif request.method == 'PUT':
+        serializer = SuperSerializer(super, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+    elif request.method == 'DELETE':
+        super.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
