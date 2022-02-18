431e30e8992c8d383ca4ff578c14fae8f2a7733d
unknown <qwhick2@gmail.com>
2022-02-18 12:15:50 -0500

    Created path In urls.py File + GET Method Functionality

--- supers/views.py
+++ supers/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import SuperSerializer
+from .models import Super
+from supers import serializers
 
 # Create your views here.
+
+@api_view(['GET', 'POST'])
+def super_list(request):
+    if request.method == 'GET':
+        supers = Super.objects.all()
+        serializer = SuperSerializer(supers, many=True)
+        return Response(serializer.data)
+
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def super_detail(request):
+    pass
